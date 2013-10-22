a344a6a68e725d6cd5988f69ae094f039868612e
Dmytro Tretiakov <dimaty@macpaw.com>
2013-10-22 11:37:02 +0300

    Changed priority queue.

--- Source/PLCrashReporter.m
+++ Source/PLCrashReporter.m
@@ -235,7 +235,7 @@ static void uncaught_exception_handler (NSException *exception) {
         return;
     }
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
         PLCrashReporter *reporter = [PLCrashReporter sharedReporter];
         
         NSError *errorGeneratingLiveReport = nil;
